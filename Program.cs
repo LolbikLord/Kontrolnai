9498cc68d703ee0dbb999599bae1e82f53bc3827
Artem0678 <darkfier39@gmail.com>
2024-04-17 22:11:32 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int arrayLength = Convert.ToInt32(Console.ReadLine());
+        string[] inputArray = new string[arrayLength];
+        FillArray(inputArray);
+        
+        string[] shortStringsArray = FilterShortStrings(inputArray);
