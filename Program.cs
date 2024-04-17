4ac0e848efe134661a56c5d698e3c55287a3c08a
Artem0678 <darkfier39@gmail.com>
2024-04-17 22:46:42 +0300

    FinalCodeVersion

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ class Program
         Console.Write("Введите количество элементов массива: ");
         int arrayLength = Convert.ToInt32(Console.ReadLine());
     string[] inputArray = new string[arrayLength];
+    FillArray(inputArray);
+    string[] shortStringsArray = FilterShortStrings(inputArray);
+    Console.WriteLine("Массив строк, длина которых меньше либо равна 3 символа:");
+    PrintArray(shortStringsArray);
+
 
     }
     static void FillArray(string[] array)
@@ -17,5 +22,43 @@ class Program
         array[i] = Console.ReadLine();
     }
 }
+    static string[] FilterShortStrings(string[] array)
+{
+    int shortStringsCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            shortStringsCount++;
+        }
+    }
+
+    string[] shortStringsArray = new string[shortStringsCount];
+    int currentIndex = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            shortStringsArray[currentIndex] = array[i];
+            currentIndex++;
+        }
+    }
+
+    return shortStringsArray;
+}
+    static void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"'{array[i]}'");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
 
 }
