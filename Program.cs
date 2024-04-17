add3aff5b3a30668f3d61112cce2d21a50465e61
Artem0678 <darkfier39@gmail.com>
2024-04-17 22:42:24 +0300

    Third

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ class Program
     {
         Console.Write("Введите количество элементов массива: ");
         int arrayLength = Convert.ToInt32(Console.ReadLine());
-        string[] inputArray = new string[arrayLength];
+    string[] inputArray = new string[arrayLength];
 
     }
+    static void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент массива:");
+        array[i] = Console.ReadLine();
+    }
+}
+
 }
